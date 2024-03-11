bbb5f40e4b9317e8bd4dc574ab9652bead9e76e1
woozziam <leejungwook0211@gmail.com>
2024-03-11 10:04:07 +0900

    first modules

--- src/motor_manager.py
+++ src/motor_manager.py
@@ -3,35 +3,25 @@ from PyQt5.QtSerialPort import QSerialPort
 class MotorManager :
     def __init__(self):
         self.serial_port = QSerialPort()
-        self.serial_port.setPortName("B2")  # 아두이노의 포트명에 맞게 변경
+        self.serial_port.setPortName("/dev/ttyACM0")  # 아두이노의 포트명에 맞게 변경
         self.serial_port.setBaudRate(QSerialPort.Baud9600)
+        
 
     def moveconveyor(self):
 
-        self.labelOn.show()
-        self.labelOn2.show()
-        self.labelOn3.show()
-        self.labelOff.hide()
-        self.labelOff2.hide()
-        self.labelOff3.hide()
-
+        print("conveyor moving")
         self.serial_port.open(QSerialPort.WriteOnly)
-        self.serial_port.write(b'S')
+        self.serial_port.write(b'R')
         self.serial_port.close()
-
-        pass
 
     def stopconveyor(self):
 
-        self.labelOn.hide()
-        self.labelOn2.hide()
-        self.labelOn3.hide()
-        self.labelOff.show()
-        self.labelOff2.show()
-        self.labelOff3.show()
-
+        print("conveyor stopped")
         self.serial_port.open(QSerialPort.WriteOnly)
-        self.serial_port.write(b'T')  
+        self.serial_port.write(b'S')  
         self.serial_port.close()
 
-        pass
+    def check_motor(self):
+        self.serial_port.open(QSerialPort.WriteOnly)
+        self.serial_port.write(b'F')
+        self.serial_port.close()
