c34c91a3aa709d73d165a8071d9d959c581087d0
gaeun0123 <cheun0928@naver.com>
2024-03-12 10:14:55 +0900

    Communication complete(mqtt, serial)
    컨베이어 가동 확인

--- src/communication/serial_communication.py
+++ src/communication/serial_communication.py
@@ -1,27 +1,44 @@
 import serial
+inputs = []
+isreturn = 0
 import time
-
-
-def write_serial_data(hub_name):
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=10)
     
+    
+def connect():
+    try:
+        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=10)
+        print("Serial port connected successfully.")
+        return ser
+    except Exception as e:
+        print(f"Failed to connect to serial port: {e}")
+        return None
+    
+    
+def disconnect(ser):
+    ser.close()
+
+
+def write_serial_data(ser, hub_name):
     if ser.isOpen():
         print("Serial port is open!")
-        tmpsend = "#S@HUB,{}#".format(hub_name) # 데이터 형식 : #보드명@데이터, {허브명}#
-        ser.write(bytes(tmpsend, "utf-8"))      # 데이터 전송
-
-        time.sleep(0.5)
-        
-        while ser.in_waiting > 0:  # 대기 중인 데이터가 있는지 확인
-            tmpmsg = ser.read(ser.in_waiting)
-            tmpdecode = tmpmsg.decode('utf-8')
-            print(tmpdecode)
-
-        ser.close()
-        
+        print(f"Sending hub_name: {hub_name}")
+            
+        if hub_name: # hub_name 값이 있는 경우에만 데이터 전송
+            data_to_send = hub_name #"#S@HUB,{}#".format(hub_name)
+            print(data_to_send)
+            
+            ser.write(bytes(data_to_send, "utf-8"))
+            time.sleep(3)  # 아두이노가 데이터를 처리할 시간
+            print("Data sent successfully.")
+                
+            # if ser.in_waiting: # 수신된 데이터가 있는지 확인
+            #     response = ser.read(ser.in_waiting).decode('utf-8')
+            #     print(f"Received response: {response}")
+            # else:
+            #     print("No response received.")    
     else:
-        print("Serial port is not open!")
+        print("Serial port is not open.")
 
 
-def get_serial_data(hub_name):
+def get_serial_data(data):
     pass
