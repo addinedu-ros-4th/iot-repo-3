9fc898bcc18d34cccb559a183bbff377e0ff3111
gaeun0123 <cheun0928@naver.com>
2024-03-12 10:14:55 +0900

    Communication complete(mqtt, serial)
    컨베이어 가동 확인

--- src/communication/communication.py
+++ src/communication/communication.py
@@ -3,13 +3,17 @@ import json
 import sys
 import os
 sys.path.append(os.path.abspath('/home/addinedu/git_ws/iot-repo-3/src/communication/database'))
-from communication.database.db_control import insert_database   # database control module
+from database.db_control import insert_database   # database control module
 from datetime import datetime
-from communication.serial_communication import write_serial_data
+import serial
+inputs = []
+isreturn = 0
+import time
 
-
-# ip_number = '192.168.219.108'           # house
+# MQtt communication
 ip_number = '192.168.0.85'                # edu
+# serial communication
+ser = serial.Serial('/dev/ttyACM1', 9600, timeout=10)
 
 
 # Connecting broker callback
@@ -32,6 +36,23 @@ def on_message(client, userdata, msg):
     split_insert(msg)
 
 
+def write_serial_data(hub_name):
+    
+    if ser.isOpen():
+        print(f"Sending hub_name: {hub_name}")
+            
+        if hub_name: # hub_name 값이 있는 경우에만 데이터 전송
+            data_to_send = "#S@HUB,{}#".format(hub_name)
+            print(f"Format data : {data_to_send}")
+            
+            ser.write(bytes(data_to_send, "utf-8"))
+            time.sleep(2) 
+            print("Data sent successfully.")
+                  
+    else:
+        print("Serial port is not open.")
+        
+        
 # For insert Database, split number
 def split_insert(msg):
     barcode = msg.payload.decode('utf-8')
@@ -44,10 +65,29 @@ def split_insert(msg):
     
     start_time = datetime.now()
     
+    # Insert
     insert_database(barcode, size_category, hub_name, treatment, start_time)
     
     # Send to Arduino board2 (serial communication) 'HubInfo'
     write_serial_data(hub_name)
+    
+    
+ 
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
--- src/communication/serial_test.py
+++
@@ -1,5 +0,0 @@
-from serial_communication import write_serial_data
-
-hub_name = "01"
-
-write_serial_data(hub_name)
