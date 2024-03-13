c478935f5627ee15207cf71b5309c78bd0121045
yjs <yjs126@gmail.com>
2024-03-13 16:28:55 +0900

    add Green LED for barcode reading success

--- src/arduino/barcodeBoard/barcodeBoard.ino
+++ src/arduino/barcodeBoard/barcodeBoard.ino
@@ -7,16 +7,16 @@
  * #240308
  *  - add server signal
  *  
- * #240312
+ * #240312_1
  *  - 1st test complete
+ * 
+ * #240312_2
+ *  - final code diet!
+ *  
+ * #240313
+ *  - Add green LED for recognize barcode reading success
  */
 #include <ESP8266WiFi.h>
-
-// test
-#define BTN D0
-#define GLED D1
-#define RLED D2
-bool flg_btn = 0;
 
 float lastTime = millis();
 
@@ -39,25 +39,12 @@ unsigned long lastMsg = 0;
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
 
-//TX
-int sigOperation = 0;
-
-//RX
-//int sigEmergency = 0;
-int flgCallback = 0;
-int sigServerCmd = 0;
-// 0 : suspend
-// 1 : operation
-int sigConvMode = 0;
-// 0 : ready(green led)
-// 1 : move(red led)
-
-bool flgStart = 0;
-
 // barcode
 #include <SoftwareSerial.h>
 SoftwareSerial GM65Serial(14,12);   // MCU(RX,TX) - (SCK,MISO)
-bool flgBarcode = 0;
+
+// LED
+#define GLED D3
 
 
 void setup_wifi() {
@@ -85,7 +72,6 @@ void setup_wifi() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  flgCallback = 1;
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
@@ -93,23 +79,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-
-  #if 0
-  if (topic == inTopicStart)
-  {
-    //a = (char)payload;
-    sigServerCmd = payload - '0';
-  }
-  else if (topic == inTopicMode)
-  {
-    //b = (char)payload;
-    sigConvMode = payload - '0';
-  }
-  else
-  {
-    //
-  }
-  #endif
 
 }
 
@@ -124,7 +93,7 @@ void reconnect() {
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish(outTopicBar, "hello world");
+      //client.publish(outTopicBar, "Board1 Connected!");
       // ... and resubscribe
       client.subscribe(inTopicStart);
       client.subscribe(inTopicMode);
@@ -144,7 +113,6 @@ void setup() {
   
   
   Serial.begin(9600);
-  //Serial.begin(115200);
   // MQTT
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   setup_wifi();
@@ -152,12 +120,10 @@ void setup() {
   client.setCallback(callback);
 
   // barcode
-  GM65Serial.begin(9600);   // GM65-아두이노간 통신, 9600은 공장출하초기값(변경가능)
+  GM65Serial.begin(9600);   // GM65 Module factory default baudrate
 
-  // test
-  pinMode(BTN,INPUT);
-  pinMode(GLED,OUTPUT);
-  digitalWrite(GLED, LOW);
+  // LED
+  pinMode(GLED, OUTPUT);
 
   Serial.println("Reset complete!");
 }
@@ -172,143 +138,31 @@ void loop()
   }
   client.loop();
 
-  // ######################################
-  #if 0
-  if (flgCallback == 0)
-  {
-    sigOperation = 1;
-    String buf = String(sigOperation);
-    char bufOp[50];
-    buf.toCharArray(bufOp, 50);
-    Serial.print("Publish message: ");
-    Serial.println(bufOp);
-    client.publish(outTopicOn, bufOp);
 
-  }
-  else if (flgCallback == 1)
-  {
-    // aleady operating....
-  }
-  else
-  {
-    // error
-  }
-
-
-  if (sigServerCmd == 1) flgStart = 1;
-  else if (sigServerCmd == 0) flgStart = 0;
-  else ; // Serial.print(command error);
-
-
-  if (flgStart == 1)
-  {
-    if (sigConvMode == 0)
-    {
-      // ready mode
-      digitalWrite(GLED, HIGH);
-      digitalWrite(RLED, LOW);
-
-      if ((currentTime - lastTime) > 1000)
-      {
-        flgBarcode = 1;
-        lastTime = currentTime;
-      }
-      else
-      {
-        currentTime = millis();
-        //
-      }
-    
-      if (flgBarcode == 1)
-      {
-        String barcodeValue = BarcodeScan();
-        char buf[50];
-        barcodeValue.toCharArray(buf, 50);
-    
-        Serial.print("Publish message: ");
-        Serial.println(buf);
-        client.publish(outTopicBar, buf);
-        //client.publish(outTopicBar, "XX04LI01");
-    
-        flgBarcode = 0;
-      }
-      else
-      {
-        //
-      }
-      
-    }
-    else if (sigConvMode == 1)
-    {
-      // move mode
-      digitalWrite(GLED, LOW);
-      digitalWrite(RLED, HIGH);
-    }
-    else ; // Serial.print(command error);
-  }
-  else
-  {
-    // suspend
-  }
-
-  #endif
-  // ######################################
-
-
-  // BTN Press mode
-  #if 0
-  int btnState = digitalRead(BTN);
-
-  if (btnState == HIGH)
-  {
-    digitalWrite(GLED, HIGH);
-
-    if (flg_btn == 0)
-    {
-      String barcodeValue = BarcodeScan();
-      
-      char buf[50];
-      barcodeValue.toCharArray(buf, 50);
-
-      Serial.print("Publish message: ");
-      Serial.println(buf);
-      client.publish(outTopicBar, buf);
-
-      flg_btn = 1;
-    }
-    else
-    {
-      
-    }
-  }
-  else
-  {
-    flg_btn = 0;
-    digitalWrite(GLED, LOW);
-  }
-  #endif
-
-
-  // 1sec period mode
+  // 2 sec period mode
   #if 1
   if ((millis() - lastTime) > 2000)
   {
+    digitalWrite(GLED, LOW);
     String barcodeValue = BarcodeScan();    
     char buf[50];
     barcodeValue.toCharArray(buf, 50);
-
 
     if (int(buf[0]) == 0)
     {
       //Serial.println("Null");
     }
+    else if (buf[7] == 0)
+    {
+      //Serial.println("data loss");
+    }
     else
     {
-      Serial.print("Publish message: ");
-      Serial.println(buf);
+      digitalWrite(GLED, HIGH);
+      //Serial.print("Publish message: ");
+      //Serial.println(buf);
       client.publish(outTopicBar, buf);
     }
-
     lastTime = millis();
   }
   else
@@ -316,8 +170,6 @@ void loop()
     //
   }
   #endif
-
-  
   
 }
 
